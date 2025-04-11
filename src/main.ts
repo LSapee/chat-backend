f88311141ab107abb4503e8191ae3a02b5bfd4c6
LSapee <dlworhks94@gmail.com>
2025-04-11 17:43:58 +0900

    alb세팅을 위한 응답시간 설정

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,11 @@ async function bootstrap() {
       credentials:true,
     }});
   const PORT = process.env.PORT||5001;
+  // ALB 설정을 위한 응답시간 설정
+  const server = app.getHttpAdapter().getHttpServer();
+  server.keepAliveTimeout = 61 * 1000; // ALB Idle Timeout보다 길게 설정
+  server.headersTimeout = 65 * 1000; // 헤더 타임아웃 추가 설정
+
   // body로 받을수 있는 용량 제한을 50mb로
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
