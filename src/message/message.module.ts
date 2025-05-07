690d7867079efef7ad63e88f104cb5efac6637c2
LSapee <dlworhks94@gmail.com>
2025-05-07 22:33:16 +0900

    authMiddleware 통과해야하는 api 추가

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -7,10 +7,12 @@ import { MessageProvider } from './message.provider';
 import { DatabaseModule } from '../database/database.module';
 import { AwsS3Service } from '../aws/aws.service';
 import { ChatGateway } from '../chat/chat.gateway';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
   imports: [UserModule,
     DatabaseModule,
+    ChatModule
   ],
   controllers: [MessageController],
   providers: [MessageService,AwsS3Service,ChatGateway,...MessageProvider],
@@ -24,6 +26,8 @@ export class MessageModule implements NestModule {
         "api/messages/users",
         "api/messages/:id",
         "api/messages/send/:id",
+        "api/messages/create-or-join/:id",
+        "api/messages/rooms/all",
       )
   }
 }
